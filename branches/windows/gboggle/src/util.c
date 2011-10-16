9d5d0d423dd9fce84e363afe44dda3920be7e6f2
vnagy <vnagy@82e3ec4a-ec32-0410-babe-c6ca9af39672>
2011-10-16 16:46:47 +0000

    created a branch for windows port
    this version is compiled in mingw successfully

---
+++ branches/windows/gboggle/src/util.c
@@ -0,0 +1,77 @@
+#ifdef HAVE_GCONF
+#include <gconf/gconf-client.h>
+#endif
+
+#include "util.h"
+
+#define GCONF_PREFIX "/apps/gboggle"
+#define GCONF_KEY_LANGUAGE GCONF_PREFIX "/language"
+#define GCONF_KEY_GAME_TIME GCONF_PREFIX "/game_time"
+
+/**
+ * returned struct is owned by the caller
+ */
+GBoggleSettings *
+load_settings (void)
+{
+#ifdef HAVE_GCONF
+    GConfClient *gconf_client = gconf_client_get_default ();
+    gchar *language = gconf_client_get_string (gconf_client, 
+                                               GCONF_KEY_LANGUAGE, 
+                                               NULL);
+    gint game_time = gconf_client_get_int (gconf_client, 
+                                          GCONF_KEY_GAME_TIME, 
+                                          NULL);
+#else
+    gchar *language = NULL;
+    gint game_time = GAME_LENGTH_SEC;
+#endif
+
+    GBoggleSettings *settings = g_new0(GBoggleSettings, 1);
+    settings->language = language;
+    if (game_time == -1) settings->game_time = 0;
+    else if (!game_time) settings->game_time = GAME_LENGTH_SEC;
+    else settings->game_time = game_time;
+    DEBUGMSG("Game time: %d\n", settings->game_time);
+
+#ifdef HAVE_GCONF
+    g_object_unref(gconf_client);
+#endif
+
+    return settings;
+}
+
+void
+save_settings(const GBoggleSettings *settings)
+{    
+#ifdef HAVE_GCONF
+    GConfClient *gconf_client = gconf_client_get_default();
+    gboolean res;
+    gint saved_game_time;
+    g_assert(settings);
+    if (settings->language) {
+        res = gconf_client_set_string(gconf_client,
+                                               GCONF_KEY_LANGUAGE, 
+                                               settings->language, 
+                                               NULL);
+        if (!res) {
+            g_warning("saving %s into gconf was failed", GCONF_KEY_LANGUAGE);
+        }
+    }
+    
+    if (settings->game_time == 0) { saved_game_time = -1; }
+    else { saved_game_time = settings->game_time; }
+     
+    res = gconf_client_set_int(gconf_client,
+                              GCONF_KEY_GAME_TIME, 
+                              saved_game_time, 
+                              NULL);
+    if (!res) {
+    	g_warning("saving %s into gconf was failed", GCONF_KEY_GAME_TIME);
+    }
+    else
+    {
+      	DEBUGMSG("Saved game time as %d\n", saved_game_time);
+	}
+#endif // HAVE_GCONF
+}
---
+++ branches/windows/gboggle/src/util.h
@@ -0,0 +1,28 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <libintl.h>
+#include <glib.h>
+
+#define _(Str) gettext (Str)
+
+#define DEBUG
+
+#ifdef DEBUG
+#define DEBUGMSG(...) g_debug(__VA_ARGS__)
+#else
+#define DEBUGMSG(...)
+#endif
+
+#define GAME_LENGTH_SEC 120
+
+typedef struct _GBoggleSettings GBoggleSettings;
+struct _GBoggleSettings {    
+    gchar *language;
+    gint game_time;
+};
+
+GBoggleSettings *load_settings(void);
+void save_settings(const GBoggleSettings *);
+
+#endif /* UTIL_H */
